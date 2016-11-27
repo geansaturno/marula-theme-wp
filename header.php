86be89f87112d7c4b03d5865bc9c68ed78c9ecf4
Gean Saturno <geansaturno@gmail.com>
2016-11-27 12:50:37 +0000

    2.2

--- header.php
+++ header.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <?php $root_dir = get_template_directory_uri(); ?>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="<?=$root_dir?>/reset.css" type="text/css" />
+        <link rel="stylesheet" href="<?=$root_dir?>/style.css" type="text/css" />
         <?php wp_head() ?>
     </head>
     <body>
--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php get_header() ?>
 
-<h1>Bem vindo!</h1>
-
-<?php if(have_posts()) :?>
-
-    <?php while(have_posts()) :?>
-        <?php the_post() ?>
-
-        <h2><?php the_title() ?></h2>
-
-        <p><?php the_content() ?></p>
-    <?php endwhile ?>
-
-<?php endif ?>
+<main class="home-main">
+    <div class="container">
+        <h1>Bem vindo ao Maluras</h1>
+        
+        <?php if(have_posts()) :?>
+            <ul class="imoveis-listagem">
+                <?php while(have_posts()) :?>
+                    <li class="imoveis-listagem-item">
+                        <?php the_post() ?>
+                
+                        <h2><?php the_title() ?></h2>
+                        <?php the_post_thumbnail()?>
+                        <p><?php the_content() ?></p>
+                    </li>
+                <?php endwhile ?>
+            </ul>
+        <?php endif ?>
+    </div>
+</main>
 
 <?php get_footer() ?>
