2e49bda924e68be868af0ef4d781c17702d47c65
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2016-12-06 14:17:51 -0200

    4.1

--- functions.php
+++ functions.php
@@ -2,16 +2,43 @@
 
 add_theme_support('post-thumbnails');
 
+function registerLabels($name, $namePlural){
+
+    $labels = array(
+        'name' => $namePlural,
+        'singular_name' => $name,
+        'view_item' => 'Ver ' . $name,
+        'edit_item' => 'Editar ' . $name,
+        'new_item' => 'Novo ' . $name,
+        'add_new_item' => 'Adicionar novo ' . $name
+    );
+
+    return $labels;
+}
+
+function taxonimies(){
+
+    $name = 'Localização';
+    $namePlural = "Localizações";
+
+    $labels = registerLabels($name, $namePlural);
+
+    $args = array(
+        'hierarchical' => true,
+        'public' => true,
+        'labels' => $labels
+    );
+
+    register_taxonomy('localizacao', 'imovel', $args);
+
+}
+
 function postTypes(){
 
     $nome = 'Imóvel';
     $nomePlural = 'Imóveis';
 
-    $labels = array(
-        'name' => $nomePlural,
-        'name_singular' => $nome,
-        'add_new_item' => 'Adicionar novo ' . $nome,
-    );
+    $labels = registerLabels($nome, $nomePlural);
 
     $supports = array(
         'title',
@@ -47,3 +74,4 @@ function geraTitle(){
 
 add_action('init', 'postTypes');
 add_action('init', 'menus');
+add_action('init', 'taxonimies');
